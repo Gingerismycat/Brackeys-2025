b570ea11af9bbf0c5b4951029cf63d3ffe27b3f8
Gingerismycat <vuk.arula@gmail.com>
2025-02-17 21:06:59 -0600

    Updated turning

--- scripts/turnable.gd
+++ scripts/turnable.gd
@@ -3,6 +3,7 @@ extends Node2D
 @onready var turnable = $"."
 @export var rotationSpeed = 0.03
 @export var wheelRadMax = 1.3
+@export var returnSpeed = 10
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -17,7 +18,7 @@ func _process(delta):
 		if turnable.rotation > -1 * wheelRadMax:
 			turnable.rotate(-1*rotationSpeed)
 	else:
-		while turnable.rotation < 0:
-			turnable.rotation+=rotationSpeed*delta
-		while turnable.rotation > 0:
-			turnable.rotation-=rotationSpeed*delta
+		if turnable.rotation < 0:
+			turnable.rotation+=returnSpeed*delta
+		if turnable.rotation > 0:
+			turnable.rotation-=returnSpeed*delta
